ff0660fe7b758d659b95926029e62d2974e6f91f
bat-stream <darshanjk202@gmail.com>
2026-09-14 13:27:20 +0530

    Add user ID to file button markup

--- handlers/messages.py
+++ handlers/messages.py
@@ -318,7 +318,13 @@ async def start(c: Client, m: Message):
             )
             return
 
-        markup = get_file_buttons(results, query, 0)
+        markup = get_file_buttons(
+            results,
+            query,
+            0,
+            m.from_user.id
+        )
+
 
         msg = await m.reply(
             f"🔍 Search results for <b>{query}</b>:",
